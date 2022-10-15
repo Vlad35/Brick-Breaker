804d9eeaa469e5c0ca1a140190cd303515a9e4e0
Vlad35 <90512038+Vlad35@users.noreply.github.com>
2022-10-15 16:34:40 +0300

    Running the game!

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package src;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Break out Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
